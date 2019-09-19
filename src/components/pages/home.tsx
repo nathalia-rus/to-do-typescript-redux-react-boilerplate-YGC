224a45de8aad19b91d7f84291fb171c1c0006f59
Nathalia Rus <nathaliarus@live.fr>
2019-09-19 13:45:42 +0100

    connect AppProps, AppState in homepage -onto refractoring redux...

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,21 +1,59 @@
 import React from "react";
 
 import Button from "../atoms/button";
+import { getTodos } from "../../redux/actions/todoActions";
+import { connect } from "react-redux";
+import { AppProps, AppState } from "../../redux/actions/interface";
 
-type HomePageProps = {
-  title: String;
-  description: String;
-};
+export class HomePage extends React.Component<AppProps, AppState> {
+  state = {
+    isLoading: false
+  };
 
-const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <Button> Submit </Button>
-    <p>
-      This website allows you to create tasks and keep track of your to-do list
-      :) :)
-    </p>
-  </div>
-);
+  simpleAction = () => {
+    this.props.getTodos();
+  };
 
-export default HomePage;
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <Button> Submit </Button>
+        <p>
+          This website allows you to create tasks and keep track of your to-do
+          list :) :)
+        </p>
+        <p>{JSON.stringify(this.props)}</p>
+        <button type="button" onClick={() => this.simpleAction}>
+          Test redux action
+        </button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: any) => ({
+  todos: state.todos
+});
+
+const mapDispatchToProps = (dispatch: function) => ({
+  getTodos: () => dispatch(getTodos())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HomePage);
+
+// const HomePage = () => (
+//   <div>
+//     <h1>Home Page</h1>
+//     <Button> Submit </Button>
+//     <p>
+//       This website allows you to create tasks and keep track of your to-do list
+//       :) :)
+//     </p>
+//   </div>
+// );
+
+// export default HomePage;
